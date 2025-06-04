663219d45e113c7a6b76f38f6dc6193a88f57106
Mohamed Farouk <mohamed2288farouk@gmail.com>
2025-06-05 01:57:01 +0300

    first commit

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:newsapp/model/News_Response.dart';
 import 'package:newsapp/model/Sources_respons.dart';
 
 class Api {
@@ -12,5 +13,16 @@ class Api {
     String responseBody = response.body;
     Map<String, dynamic> json = jsonDecode(responseBody);
     return SourcesRespons.fromJson(json);
+  }
+
+  // https://newsapi.org/v2/everything?q=bitcoin&apiKey=f2a0be9f61c548cb9a51fa6a00d7d830
+  static Future<NewsResponse> getNews() async {
+    Uri url = Uri.https("newsapi.org", "/v2/everything", {
+      "apikey": "f2a0be9f61c548cb9a51fa6a00d7d830",
+    });
+    var response = await http.get(url);
+    var responseBody = response.statusCode;
+    var json = jsonDecode(responseBody as String);
+    return NewsResponse.fromJson(json);
   }
 }
