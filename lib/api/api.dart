b608611695e2e1cf0eb3bef3b35d5d4665af1cb5
Mohamed Farouk <mohamed2288farouk@gmail.com>
2025-06-04 15:17:46 +0300

    finished get_sources Function.

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,3 +1,16 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:newsapp/model/Sources_respons.dart';
 
-class Api {}
+class Api {
+  Future<SourcesRespons> getSource() async {
+    Uri url = Uri.https("newsapi.org", "/v2/top-headlines/sources", {
+      "apiKey": "f2a0be9f61c548cb9a51fa6a00d7d830",
+    });
+
+    var response = await http.get(url);
+    String responseBody = response.body;
+    Map<String, dynamic> json = jsonDecode(responseBody);
+    return SourcesRespons.fromJson(json);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/Home_Screen.dart';
+import 'package:newsapp/api/api.dart';
+import 'package:newsapp/model/Sources_respons.dart';
 
-void main() {
+Future<void> main() async {
+  Api api = Api();
+  SourcesRespons sourcesRespons = await api.getSource();
+  print(sourcesRespons.sources!.length);
   runApp(const NewsApp());
 }
 
